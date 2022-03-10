b656d71da0f0c2c66b18e310618368b27cd2017d
santoshgudeti <83604184+santoshgudeti@users.noreply.github.com>
2022-03-10 11:48:01 +0530

    Add files via upload

---
+++ basicdata.js
@@ -0,0 +1,17 @@
+
+let name="Akash";
+let branch="Hyd";
+
+let getbranch=function()
+{
+    return branch;
+
+}
+let setbranch=function(newbranch)
+{
+     branch=newbranch;
+
+}
+export{getbranch,setbranch}
+
+ 
---
+++ def.js
@@ -0,0 +1,10 @@
+let branch ="hyd";
+let location={let getBranch:function()}
+{
+    return branch;
+}
+setBranch=function(newBranch)
+{
+    branch=newBranch
+}
+export default location;
